03864720227ca6f6269f5b8381f1fdc9bb579a91
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-11-07 15:51:19 +0100

    style: adhere to Obsidian style guide

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ export default class ObsidianNothing extends Plugin {
 	async onload() {
 		this.addCommand({
 			id: "nothing-noop",
-			name: "Do Nothing (no-op)",
+			name: "Do nothing (no-op)",
+			// biome-ignore lint/nursery/noEmptyBlockStatements: well, it's a no-op
 			callback: () => {},
 		});
 	}
