33252d63398d7a3589c131de6fa9027e9c339bfd
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-09 20:56:24 +0100

    chore: update package versions

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { Plugin } from "obsidian";
 
 export default class ObsidianNothing extends Plugin {
-	async onload() {
+	override onload() {
 		this.addCommand({
 			id: "nothing-noop",
 			name: "Do nothing (no-op)",
-			// biome-ignore lint/nursery/noEmptyBlockStatements: well, it's a no-op
+			// biome-ignore lint/suspicious/noEmptyBlockStatements: it's deliberately a no-op
 			callback: () => {},
 		});
 	}
